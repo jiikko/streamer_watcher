5d7866c45ae8687f3b5efbce9319e395807f79ce
jiikko <n905i.1214@gmail.com>
2024-06-23 14:18:37 +0900

    交差テーブルを更新できるようにする

--- app/controllers/streamers_controller.rb
+++ app/controllers/streamers_controller.rb
@@ -1,5 +1,22 @@
 class StreamersController < ApplicationController
   def edit
     @talent = Talent.find(params[:talent_id])
+    @streamer = @talent.streamers.find(params[:id])
+  end
+
+  def update
+    @talent = Talent.find(params[:talent_id])
+    @streamer = @talent.streamers.find(params[:id])
+    if @streamer.update(streamer_params)
+      redirect_to talent_streaming_platforms_path(@talent), notice: 'Streamer was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def streamer_params
+    params.require(:streamer).permit(:streamer_key, :notify, :download_live_stream)
   end
 end
