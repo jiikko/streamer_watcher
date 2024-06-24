f5d2d803df8028f2eb09c9aa319a5322aa5c390e
jiikko <n905i.1214@gmail.com>
2024-06-24 22:17:50 +0900

    エラーコードも見る

--- app/models/describe_streaming.rb
+++ app/models/describe_streaming.rb
@@ -35,13 +35,14 @@ class DescribeStreaming
   end
 
   def execute
-    file_name_part = @url.match(%r{/([^/]+?)$})[1]
-    command = "yt-dlp \"#{@url}\" --skip-download --write-info-json -o '#{Rails.root}/live-stream#{file_name_part}'"
-    _stdout, stderr, _status = Open3.capture3(command)
-    return NotLiveResult.new if stderr.include?('The channel is not currently live')
+    file_name_part = @url.match(%r{/([^/]+?)$})[1].gsub(':', '-')
+    command = "yt-dlp \"#{@url}\" --skip-download --write-info-json -o '#{Rails.root}/tmp/live-stream#{file_name_part}'"
+    Rails.logger.info "[SHELL] #{command}"
+    _stdout, stderr, status = Open3.capture3(command)
+    return NotLiveResult.new if stderr.include?('The channel is not currently live') || !status.success?
 
-    json = JSON.parse(File.read("#{Rails.root}/live-stream#{file_name_part}.info.json"))
-    FileUtils.rm_f("#{Rails.root}/live-stream#{file_name_part}.info.json")
+    json = JSON.parse(File.read("#{Rails.root}/tmp/live-stream#{file_name_part}.info.json"))
+    FileUtils.rm_f("#{Rails.root}/tmp/live-stream#{file_name_part}.info.json")
     Result.new(json)
   end
 end
