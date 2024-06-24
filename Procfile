6da4bec17792a4f231adf003748a7afd571e0173
jiikko <n905i.1214@gmail.com>
2024-06-24 09:13:26 +0900

    FAST

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: bundle exec rails s -p 5001
-worker1: IS_SCHEDULER=1 bundle exec sidekiq
-worker2: bundle exec sidekiq
+web: FAST=1 bundle exec rails s -p 5001
+worker1: FAST=1 IS_SCHEDULER=1 bundle exec sidekiq
+worker2: FAST=1 bundle exec sidekiq
 redis: redis-server ./redis.development.conf
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,10 +6,14 @@ Rails.application.configure do
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.enable_reloading = true
 
-  # Do not eager load code on boot.
-  config.eager_load = false
+  if ENV['FAST']
+    config.enable_reloading = false
+    config.eager_load = true
+  else
+    config.enable_reloading = true
+    config.eager_load = false
+  end
 
   # Show full error reports.
   config.consider_all_requests_local = true
